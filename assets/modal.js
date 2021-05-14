52f65f3a6b5f3db641df87d72b340c4045943904
shrijana <shrijana1991@gmail.com>
2021-05-14 18:56:43 +0545

    update nft

---
+++ assets/modal.js
@@ -0,0 +1,27 @@
+function display_modal(modalID) {
+    // Get the modal
+    var modal = document.getElementById(modalID);
+
+    // Get the button that opens the modal
+    // var btn = document.getElementById(modalID);
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    // btn.onclick = function() {
+    modal.style.display = "block";
+    // }
+
+    // When the user clicks on <span> (x), close the modal
+    // span.onclick = function() {
+    // modal.style.display = "none";
+    // }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
