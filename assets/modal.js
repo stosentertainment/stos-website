1490248bd20d8daa74ac7994eec6953b55e7a463
shrijana <shrijana1991@gmail.com>
2021-05-14 21:19:45 +0545

    add discord link

--- assets/modal.js
+++ assets/modal.js
@@ -14,9 +14,9 @@ function display_modal(modalID) {
     // }
 
     // When the user clicks on <span> (x), close the modal
-    // span.onclick = function() {
-    // modal.style.display = "none";
-    // }
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
