cab1535fe5100870324b1e1fe6ccdc9c0b401e7e
shrijana <shrijana1991@gmail.com>
2021-05-16 17:37:57 +0545

    pause and play video

--- assets/modal.js
+++ assets/modal.js
@@ -1,27 +1,23 @@
 function display_modal(modalID) {
-    // Get the modal
     var modal = document.getElementById(modalID);
 
-    // Get the button that opens the modal
-    // var btn = document.getElementById(modalID);
-
-    // Get the <span> element that closes the modal
     var specific_span = document.getElementsByClassName(modalID+"_close")[0];
 
-    // When the user clicks on the button, open the modal
-    // btn.onclick = function() {
-    modal.style.display = "block";
-    // }
 
-    // When the user clicks on <span> (x), close the modal
+    jQuery('#'+modalID+' video').prop('muted', false);
+    jQuery('#'+modalID+' video')[0].play();
+
+    modal.style.display = "block";
+
     specific_span.onclick = function() {
         modal.style.display = "none";
+        jQuery('#'+modalID+' video')[0].pause();
     }
 
-    // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
+            jQuery('#'+modalID+' video')[0].pause();
         }
     }
 }
